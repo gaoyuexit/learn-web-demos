7a561e88f36ca671d882fe7ee9a996fc08f6f66c
gaoyu <gaoyuexit@163.com>
2018-01-08 16:23:25 +0800

    modify ui

--- ball-animation/main.js
+++ ball-animation/main.js
@@ -27,7 +27,6 @@ function draw() {
 function clear() {
   ctx.fillStyle = 'rgba(255,255,255,0.3)';
   ctx.fillRect(0,0,canvas.width,canvas.height);
-  ctx.strokeRect(0, 0, canvas.width, canvas.height);
 }
 
 document.body.onload = function() {
